9145054459df084018f02ebfa33dcdc9ba95b2b0
hcws111 <160553666+hcws111@users.noreply.github.com>
2024-04-06 17:40:12 +0800

    更新04

--- js/addFrontNavbar.js
+++ js/addFrontNavbar.js
@@ -4,11 +4,11 @@ var html='<section style="background-color: #eee7e3;">'+
             '<section>'+
                 '<nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark navbar-dark" style="height: 100px;">'+
                     '<div class="container">'+
-                        '<a class="navbar-brand offset-2" href="../../f_index.html"><img src="../../images/front/bagel.png" width="60" alt="" class="iconNavBar"></a>'+
+                        '<a class="navbar-brand offset-2" href="../../index.html"><img src="../../images/front/bagel.png" width="60" alt="" class="iconNavBar"></a>'+
                     '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>'+
                     '<div class="collapse navbar-collapse" id="navbarSupportedContent">'+     
                         '<ul class="navbar-nav me-auto mb-2 mb-lg-0">'+
-                            '<li class="nav-item"><a class="nav-link" aria-current="page" href="../../f_index.html">首頁</a></li>'+
+                            '<li class="nav-item"><a class="nav-link" aria-current="page" href="../../index.html">首頁</a></li>'+
                             '<li class="nav-item"><a class="nav-link" aria-current="page" href="f_product_info.html">商品選購</a></li>'+
                             '<li class="nav-item" id="navItem_member"><a class="nav-link" aria-current="page" href="f_member_info.html">會員專區</a></li>'+
                         '</ul>'+    
